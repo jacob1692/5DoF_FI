6f1412df07e88557f9375611e7a209767122c146
jacob1692 <jacob.hernandezsanchez@epfl.ch>
2019-01-30 14:33:21 +0100

    add newest message

--- libraries/ros_lib/custom_msgs/FootOutputMsg.h
+++ libraries/ros_lib/custom_msgs/FootOutputMsg.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include "ros/msg.h"
+#include "ros/time.h"
 
 namespace custom_msgs
 {
@@ -12,6 +13,10 @@ namespace custom_msgs
   class FootOutputMsg : public ros::Msg
   {
     public:
+      typedef ros::Time _stamp_type;
+      _stamp_type stamp;
+      typedef int16_t _id_type;
+      _id_type id;
       typedef float _x_type;
       _x_type x;
       typedef float _y_type;
@@ -32,10 +37,22 @@ namespace custom_msgs
       _Ttheta_type Ttheta;
       typedef float _Tpsi_type;
       _Tpsi_type Tpsi;
+      typedef float _vx_type;
+      _vx_type vx;
+      typedef float _vy_type;
+      _vy_type vy;
+      typedef float _wphi_type;
+      _wphi_type wphi;
+      typedef float _wtheta_type;
+      _wtheta_type wtheta;
+      typedef float _wpsi_type;
+      _wpsi_type wpsi;
       typedef int16_t _state_type;
       _state_type state;
 
     FootOutputMsg():
+      stamp(),
+      id(0),
       x(0),
       y(0),
       phi(0),
@@ -46,6 +63,11 @@ namespace custom_msgs
       Tphi(0),
       Ttheta(0),
       Tpsi(0),
+      vx(0),
+      vy(0),
+      wphi(0),
+      wtheta(0),
+      wpsi(0),
       state(0)
     {
     }
@@ -53,6 +75,24 @@ namespace custom_msgs
     virtual int serialize(unsigned char *outbuffer) const
     {
       int offset = 0;
+      *(outbuffer + offset + 0) = (this->stamp.sec >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (this->stamp.sec >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (this->stamp.sec >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (this->stamp.sec >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->stamp.sec);
+      *(outbuffer + offset + 0) = (this->stamp.nsec >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (this->stamp.nsec >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (this->stamp.nsec >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (this->stamp.nsec >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->stamp.nsec);
+      union {
+        int16_t real;
+        uint16_t base;
+      } u_id;
+      u_id.real = this->id;
+      *(outbuffer + offset + 0) = (u_id.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_id.base >> (8 * 1)) & 0xFF;
+      offset += sizeof(this->id);
       union {
         float real;
         uint32_t base;
@@ -154,6 +194,56 @@ namespace custom_msgs
       *(outbuffer + offset + 3) = (u_Tpsi.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->Tpsi);
       union {
+        float real;
+        uint32_t base;
+      } u_vx;
+      u_vx.real = this->vx;
+      *(outbuffer + offset + 0) = (u_vx.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_vx.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_vx.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_vx.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->vx);
+      union {
+        float real;
+        uint32_t base;
+      } u_vy;
+      u_vy.real = this->vy;
+      *(outbuffer + offset + 0) = (u_vy.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_vy.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_vy.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_vy.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->vy);
+      union {
+        float real;
+        uint32_t base;
+      } u_wphi;
+      u_wphi.real = this->wphi;
+      *(outbuffer + offset + 0) = (u_wphi.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_wphi.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_wphi.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_wphi.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->wphi);
+      union {
+        float real;
+        uint32_t base;
+      } u_wtheta;
+      u_wtheta.real = this->wtheta;
+      *(outbuffer + offset + 0) = (u_wtheta.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_wtheta.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_wtheta.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_wtheta.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->wtheta);
+      union {
+        float real;
+        uint32_t base;
+      } u_wpsi;
+      u_wpsi.real = this->wpsi;
+      *(outbuffer + offset + 0) = (u_wpsi.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_wpsi.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_wpsi.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_wpsi.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->wpsi);
+      union {
         int16_t real;
         uint16_t base;
       } u_state;
@@ -167,6 +257,25 @@ namespace custom_msgs
     virtual int deserialize(unsigned char *inbuffer)
     {
       int offset = 0;
+      this->stamp.sec =  ((uint32_t) (*(inbuffer + offset)));
+      this->stamp.sec |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      this->stamp.sec |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      this->stamp.sec |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      offset += sizeof(this->stamp.sec);
+      this->stamp.nsec =  ((uint32_t) (*(inbuffer + offset)));
+      this->stamp.nsec |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      this->stamp.nsec |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      this->stamp.nsec |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      offset += sizeof(this->stamp.nsec);
+      union {
+        int16_t real;
+        uint16_t base;
+      } u_id;
+      u_id.base = 0;
+      u_id.base |= ((uint16_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_id.base |= ((uint16_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      this->id = u_id.real;
+      offset += sizeof(this->id);
       union {
         float real;
         uint32_t base;
@@ -278,6 +387,61 @@ namespace custom_msgs
       this->Tpsi = u_Tpsi.real;
       offset += sizeof(this->Tpsi);
       union {
+        float real;
+        uint32_t base;
+      } u_vx;
+      u_vx.base = 0;
+      u_vx.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_vx.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_vx.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_vx.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->vx = u_vx.real;
+      offset += sizeof(this->vx);
+      union {
+        float real;
+        uint32_t base;
+      } u_vy;
+      u_vy.base = 0;
+      u_vy.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_vy.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_vy.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_vy.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->vy = u_vy.real;
+      offset += sizeof(this->vy);
+      union {
+        float real;
+        uint32_t base;
+      } u_wphi;
+      u_wphi.base = 0;
+      u_wphi.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_wphi.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_wphi.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_wphi.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->wphi = u_wphi.real;
+      offset += sizeof(this->wphi);
+      union {
+        float real;
+        uint32_t base;
+      } u_wtheta;
+      u_wtheta.base = 0;
+      u_wtheta.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_wtheta.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_wtheta.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_wtheta.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->wtheta = u_wtheta.real;
+      offset += sizeof(this->wtheta);
+      union {
+        float real;
+        uint32_t base;
+      } u_wpsi;
+      u_wpsi.base = 0;
+      u_wpsi.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_wpsi.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_wpsi.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_wpsi.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->wpsi = u_wpsi.real;
+      offset += sizeof(this->wpsi);
+      union {
         int16_t real;
         uint16_t base;
       } u_state;
@@ -290,7 +454,7 @@ namespace custom_msgs
     }
 
     const char * getType(){ return "custom_msgs/FootOutputMsg"; };
-    const char * getMD5(){ return "8f7a23844c96aae47b26e29ed7083664"; };
+    const char * getMD5(){ return "d8e84508485f8ff66f85ecd7bdcbd77f"; };
 
   };
 
